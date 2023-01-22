fa31562fd2f36c89778035c84f488b7e3059df04
TommyTS31 <kwongkin.tam@gmail.com>
2023-01-22 18:33:45 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const bodyParser = require("body-parser");
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan("combined"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.send("App is working"));
+
+app.listen(3000, () => console.log("API listening on port 3000!"));
+
+module.exports = {
+  app,
+};
